39a805e87bb2fa8a8edf593b1fdd13e5e25fbca6
simontessard <simon.tessard@gmail.com>
2024-05-17 14:36:22 +0200

    Header mobile integration

--- src/app/partials/Header.js
+++ src/app/partials/Header.js
@@ -1,30 +1,117 @@
+"use client";
 import Logo from "@/app/assets/img/Logo.png";
 import Image from "next/image";
 import DesktopNav from "@/app/partials/DesktopNav";
+import {useLayoutEffect, useState, useRef, useEffect} from "react";
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import Link from "next/link";
 
 export function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const tl = useRef(null);
+
+    const nav = useRef(null);
+    const header = useRef(null);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    useEffect(() => {
+        if (isOpen) {
+            tl.current.play();
+        } else {
+            tl.current.reverse();
+        }
+    }, [isOpen]);
+
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+            gsap.registerPlugin(gsap, ScrollTrigger);
+            gsap.set(nav.current, {zIndex: 30, yPercent: -150});
+
+            // Hamburger icon animation
+            tl.current = gsap
+                .timeline({paused: true})
+                // Hamburger icon animation
+                .to('.hamburgerLines', {
+                    xPercent: 100,
+                    stagger: 0.05,
+                    duration: 0.4,
+                })
+                // Cross icon animation
+                .set('.crossButton', {display: 'block'}, 0.5)
+                .to('.cross:nth-child(1)', {
+                    x: '1.5rem',
+                    y: '1rem',
+                    duration: 0.3
+                }, 0.5)
+                .to('.cross:nth-child(2)', {
+                    x: '-1.5rem',
+                    y: '1rem',
+                    duration: 0.3,
+                }, 0.6)
+                // Nav animation
+                .to(nav.current, {
+                    yPercent: 0,
+                    duration: 0.6,
+                    ease: "power1.inOut",
+                }, 0.4)
+                .from('.navElement', {
+                    yPercent: 100,
+                    stagger: 0.2,
+                    duration: 0.4,
+                }, 0.9);
+
+            // Show header on scroll
+            const showAnim = gsap.from(header.current, {
+                yPercent: -100,
+                paused: true,
+                duration: 0.4
+            }).progress(1);
+
+            ScrollTrigger.create({
+                start: "top top",
+                end: "max",
+                onUpdate: (self) => {
+                   self.direction === -1 ? showAnim.play() : showAnim.reverse();
+                }
+            });
+        });
+        return () => ctx.revert();
+    }, []);
+
     return (
-        <header>
-            <span className="hidden absolute right-16 top-14 text-2xl px-2">🇫🇷</span>
-            <div className="hidden bg-black p-2">
-                <div className="container bg-black py-4 px-4 md:px-6 h-16 flex justify-between items-center rounded">
-                    <Image
-                        src={Logo}
-                        width={20}
-                        height={20}
-                        alt="Logo S"
-                    />
-                    <nav className="md:w-1/3 text-sm text-white uppercase">
-                        <ul className="flex flex-wrap justify-between w-full gap-4">
-                            <li>Projects</li>
-                            <li>Skills</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                        </ul>
-                    </nav>
-                </div>
+        <header className="fixed z-40 w-full top-0 md:hidden">
+            <div ref={header} className="container relative z-40 bg-black py-4 px-6 h-16 flex justify-between items-center">
+                <Image
+                    src={Logo}
+                    width={20}
+                    height={20}
+                    alt="Logo S"
+                />
+                    <button onClick={handleClick} className="relative w-6 h-5 overflow-hidden">
+                        <div className="hamburgerLines absolute top-0 text-white h-0.5 bg-white w-6"></div>
+                        <div className="hamburgerLines absolute top-2 text-white h-0.5 bg-white w-6"></div>
+                        <div className="hamburgerLines absolute top-4 text-white h-0.5 bg-white w-6"></div>
+                    </button>
+                    <button onClick={handleClick} className="crossButton hidden relative w-6 h-6 overflow-hidden">
+                        <div className="cross absolute -top-2 -left-full text-white h-0.5 bg-white w-6 rotate-45"></div>
+                        <div className="cross absolute -top-2 -right-full text-white h-0.5 bg-white w-6 rotate-[135deg]"></div>
+                    </button>
             </div>
-            <DesktopNav />
+            <div ref={nav} className="bg-black">
+                <ul
+                    className="container flex flex-col gap-14 items-center justify-center h-dvh pb-16 bg-black text-white text-5xl">
+                    <Link href={"/"} onClick={handleClick} className="overflow-hidden"><p
+                        className="navElement">Accueil</p></Link>
+                    <Link href={"/projects"} onClick={handleClick} className="overflow-hidden"><p
+                        className="navElement">Projets</p></Link>
+                </ul>
+            </div>
+            <DesktopNav/>
         </header>
     );
 }
