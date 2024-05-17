7390762c4dd41aaefa51cfa94c235c90e5bcd9e4
simontessard <simon.tessard@gmail.com>
2024-05-17 15:35:46 +0200

    Changed page transition color

--- src/app/utils/PageTransition.js
+++ src/app/utils/PageTransition.js
@@ -27,10 +27,10 @@ export default function Template({ children }) {
     }, [])
     return (
         <div>
-            <div id="banner-1" className="min-h-screen bg-white z-10 fixed top-0 left-0 w-1/4"/>
-            <div id="banner-2" className="min-h-screen bg-white z-10 fixed top-0 left-1/4 w-1/4"/>
-            <div id="banner-3" className="min-h-screen bg-white z-10 fixed top-0 left-2/4 w-1/4"/>
-            <div id="banner-4" className="min-h-screen bg-white z-10 fixed top-0 left-3/4 w-1/4"/>
+            <div id="banner-1" className="min-h-screen bg-black z-10 fixed top-0 left-0 w-1/4"/>
+            <div id="banner-2" className="min-h-screen bg-black z-10 fixed top-0 left-1/4 w-1/4"/>
+            <div id="banner-3" className="min-h-screen bg-black z-10 fixed top-0 left-2/4 w-1/4"/>
+            <div id="banner-4" className="min-h-screen bg-black z-10 fixed top-0 left-3/4 w-1/4"/>
             {children}
         </div>
     )
