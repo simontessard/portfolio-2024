b4f0ea08a62667f0b2a7cfd5de8a1224e2554991
simontessard <simon.tessard@gmail.com>
2024-05-18 12:36:24 +0200

    New animation

--- src/app/pages/home/sections/WorkWithMe.js
+++ src/app/pages/home/sections/WorkWithMe.js
@@ -21,20 +21,61 @@ export function WorkWithMe() {
                 },
                 y: -75,
             });
+            gsap.to('.baseCube', {
+                scrollTrigger: {
+                    trigger: section.current,
+                    start: "top 42%",
+                    end: "400",
+                    scrub: 1,
+                },
+                yPercent : -200.5
+            });
+            gsap.to('.blackSquare', {
+                scrollTrigger: {
+                    trigger: section.current,
+                    start: "top 39%",
+                    end: "350",
+                    scrub: 1,
+                },
+                yPercent : 200.5
+            });
+
         });
     }, []);
   return (
-    <section ref={section} className={"bg-white py-16 md:py-28 max-md:mb-8"}>
-        <div className="container mx-auto text-black">
-                <h2 className="text-3xl md:text-8xl font-bold mb-8 md:mb-20 font-marbry">Travaillons ensemble</h2>
-                <p className={"text-sm md:text-xl mb-4 md:mb-6"}>Vous avez un projet sur lequel vous aimeriez que je travaille ? Vous avez besoin de
-                    mes compétences pour un projet ?</p>
-                <p className={"text-sm md:text-xl mb-4 md:mb-6"}>Je suis disponible pour des missions freelance ou en CDI.</p>
-                <p className={"text-sm md:text-xl mb-8 md:mb-12"}>N'hésitez pas à me contacter pour en discuter.</p>
-                <div className={"max-md:mx-auto md:ml-auto w-fit"}>
-                    <ContactButton href={"#"} text={"Contactez moi"}/>
-                </div>
-        </div>
-    </section>
+      <section ref={section} className={"relative bg-white py-16 md:py-28 max-md:mb-8 overflow-hidden"}>
+          <div className="container text-black">
+              <h2 className="text-3xl md:text-8xl font-bold mb-8 md:mb-20 font-marbry">Travaillons ensemble</h2>
+              <div className="text-sm md:text-lg">
+                  <p className={"mb-4 md:mb-6"}>Vous avez un projet sur lequel vous aimeriez que je
+                      travaille ? Vous avez besoin de mes compétences pour un projet ?</p>
+                  <p className={"mb-4 md:mb-6"}>Je suis disponible pour des missions freelance ou en
+                      CDI.</p>
+                  <p className={"mb-8 md:mb-12"}>N'hésitez pas à me contacter pour en discuter.</p>
+              </div>
+              <div className={"max-md:mx-auto w-fit"}>
+                  <ContactButton href={"#"} text={"Contactez moi"}/>
+              </div>
+          </div>
+          <div className={"baseCube max-md:hidden absolute -bottom-40 right-32 flex items-center justify-center border-2 border-black size-40"}>
+              <svg className="size-10" viewBox="0 0 24 24" strokeWidth="1.5" width="24" height="24" color="#000000">
+                  <defs>
+                  </defs>
+                  <circle className="cls-6374f8d9b67f094e4896c670-1" cx="12" cy="7.25" r="5.73"></circle>
+                  <path className="cls-6374f8d9b67f094e4896c670-1"
+                        d="M1.5,23.48l.37-2.05A10.3,10.3,0,0,1,12,13h0a10.3,10.3,0,0,1,10.13,8.45l.37,2.05"></path>
+              </svg>
+          </div>
+          <div
+              className={"blackSquare max-md:hidden absolute -top-40 right-32 flex items-center justify-center size-40 border-2 border-black bg-black"}>
+              <svg className="fill-white size-10" viewBox="0 0 24 24" strokeWidth="1.5" width="24" height="24">
+                  <defs>
+                  </defs>
+                  <circle className="cls-6374f8d9b67f094e4896c670-1" cx="12" cy="7.25" r="5.73"></circle>
+                  <path className="cls-6374f8d9b67f094e4896c670-1"
+                        d="M1.5,23.48l.37-2.05A10.3,10.3,0,0,1,12,13h0a10.3,10.3,0,0,1,10.13,8.45l.37,2.05"></path>
+              </svg>
+          </div>
+      </section>
   );
 }
