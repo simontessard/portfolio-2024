86be9c665e60d9d7da158eeb3be8a1ae064f2feb
simontessard <simon.tessard@gmail.com>
2024-07-27 10:05:47 +0200

    Improved performance

--- src/app/project/Description.js
+++ src/app/project/Description.js
@@ -1,7 +1,8 @@
 "use client";
-import { useLayoutEffect, useRef } from 'react'
+import { useRef } from 'react'
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import gsap from 'gsap';
+import {useGSAP} from "@gsap/react";
 
 export default function Index({skills}) {
     return (
@@ -14,46 +15,48 @@ export default function Index({skills}) {
 function AnimatedText({children}) {
     const text = useRef(null);
 
-    useLayoutEffect( () => {
-        gsap.registerPlugin(ScrollTrigger);
+    useGSAP(
+        () => {
+            gsap.registerPlugin(ScrollTrigger);
 
-        let mm = gsap.matchMedia();
+            let mm = gsap.matchMedia();
 
-        // Desktop setup
-        mm.add("(min-width: 800px)", () => {
-            gsap.fromTo(text.current, {
-                    left: "-14rem",
-            },
-            {
-                scrollTrigger: {
-                    trigger: text.current,
-                    scrub: true,
-                    start: "bottom bottom",
-                    end: "+=400px bottom",
-                },
-                opacity: 1,
-                left: "0",
-                ease: "power3.Out"
+            // Desktop setup
+            mm.add("(min-width: 800px)", () => {
+                gsap.fromTo(text.current, {
+                        left: "-14rem",
+                    },
+                    {
+                        scrollTrigger: {
+                            trigger: text.current,
+                            scrub: true,
+                            start: "bottom bottom",
+                            end: "+=400px bottom",
+                        },
+                        opacity: 1,
+                        left: "0",
+                        ease: "power3.Out"
+                    });
             });
-        });
 
-        // Mobile setup
-        mm.add("(max-width: 799px)", () => {
-            gsap.fromTo(text.current, {
-                left: "-6rem",
-            },{
-                scrollTrigger: {
-                    trigger: text.current,
-                    scrub: true,
-                    start: "bottom bottom",
-                    end: "+=400px bottom",
-                },
-                opacity: 1,
-                left: "1rem",
-                ease: "power3.Out"
+            // Mobile setup
+            mm.add("(max-width: 799px)", () => {
+                gsap.fromTo(text.current, {
+                    left: "-6rem",
+                },{
+                    scrollTrigger: {
+                        trigger: text.current,
+                        scrub: true,
+                        start: "bottom bottom",
+                        end: "+=400px bottom",
+                    },
+                    opacity: 1,
+                    left: "1rem",
+                    ease: "power3.Out"
+                });
             });
-        });
-    }, [])
+        }
+    )
 
     return <p ref={text} className={"relative opacity-0 flex gap-4 md:gap-6 mb-4 md:mb-8"}>
                 <span className="text-xs md:text-sm font-mono md:pt-1.5">
--- src/app/project/SquareImage.js
+++ src/app/project/SquareImage.js
@@ -32,8 +32,7 @@ export function SquareImage({src}) {
 
     return (
         <div ref={imgRef} className={"img-container relative group overflow-hidden max-md:mx-auto max-md:max-h-64 md:max-w-lg w-full h-full md:aspect-square mb-6 md:mb-8"}>
-            <img loading="lazy"
-                className={"md:absolute md:top-0 md:left-1/2 w-auto h-full aspect-square object-cover md:-translate-x-1/2 origin-center"}
+            <img className={"md:absolute md:top-0 md:left-1/2 w-auto h-full aspect-square object-cover md:-translate-x-1/2 origin-center"}
                 src={src} alt="random"/>
         </div>
     )
